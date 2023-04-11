09750b4363936b6e4b9568632f10ddc174ddc63c
tctatxt <tammyvisca08@gmail.com>
2023-04-11 12:42:13 +0700

    selesai yey

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,15 +7,17 @@
     @foreach ($arrmakanan as $mkn)
     <a href="makanan/{{$mkn["slug"]}}" class="container is-widescreen">
         @if ($mkn["status"] == 'R')
-        <div class="notification has-background-grey-light has-text-primary-light mb-3">
+        <div class="notification has-background-grey-light has-text-primary-light mb-3 is-flex is-flex-direction-row is-align-content-center">
         @elseif ($mkn["status"] == 'SR')
         <div class="notification has-background-warning has-text-dark mb-3">
         @else
         <div class="notification has-background-danger-dark has-text-primary-light mb-3">
         @endif
+        <div class="text">
             <h1 class="is-size-2 has-text-weight-bold">{{$mkn["title"]}}</h1>
             <h3 class="is-underlined has-text-weight-semibold">{{$mkn["price"]}}</h3>
             <p>{{$mkn["desc"]}}</p>
+        </div>
         </div>
     </a>
     @endforeach
